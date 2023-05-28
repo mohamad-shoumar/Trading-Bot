1fa738eef3fab6c5fb20b7e516903aec1db3223e
MoeShoumar <mks38@mail.aub.edu>
2023-05-28 21:02:10 +0300

    fetch data directly from binance testnetwork

--- data/fetch.py
+++ data/fetch.py
@@ -1,23 +1,39 @@
-import MetaTrader5 as mt5
-from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-account = int(69956869)
+load_dotenv()
 
-mt5.initialize()
-authorized = mt5.login(account)
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
 
-if authorized:
-    print("Connected: Connecting to MT5 Client")
-else:
-    print(
-        "Failed to connect at account #{}, error code: {}".format(
-            account, mt5.last_error()
-        )
-    )
 
-utc_from = datetime(2023, 6, 20)
-utc_to = datetime(2023, 6, 27)
-rates = mt5.copy_rates_range("EURUSD", mt5.TIMEFRAME_H4, utc_from, utc_to)
+from binance.spot import Spot
 
-for rate in rates:
-    print(rate)
+client = Spot()
+
+# Get server timestamp
+print(client.time())
+# Get klines of BTCUSDT at 1m interval
+print(client.klines("BTCUSDT", "1m"))
+# Get last 10 klines of BNBUSDT at 1h interval
+print(client.klines("BNBUSDT", "1h", limit=10))
+
+# API key/secret are required for user data endpoints
+client = Spot(api_key="<api_key>", api_secret="<api_secret>")
+
+# Get account and balance information
+print(client.account())
+
+# Post a new order
+params = {
+    "symbol": "BTCUSDT",
+    "side": "SELL",
+    "type": "LIMIT",
+    "timeInForce": "GTC",
+    "quantity": 0.002,
+    "price": 9500,
+}
+
+response = client.new_order(**params)
+print(response)
+print("hello")
